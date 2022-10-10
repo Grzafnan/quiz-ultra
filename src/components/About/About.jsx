ac6a55b69c983a5c5e28888ce5e44e8a5ae156ed
Grzafnan <Grzafnan>
2022-10-11 02:49:19 +0600

    added about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import contact from "../../assets/contact.json";
+import Lottie from "lottie-react";
 
 const About = () => {
   return (
-    <div>
-      <h3>About</h3>
+    <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 items-center md:px-12 lg:px-16 xl:px-32 ">
+      <div className="flex flex-col justify-between">
+        <div className="space-y-2">
+          <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
+            Let's talk!
+          </h2>
+          <div className="dark:text-gray-400">
+            Vivamus in nisl metus? Phasellus.
+          </div>
+        </div>
+        <Lottie
+          className=" h-52 md:h-full"
+          animationData={contact}
+          loop={true}
+        />
+      </div>
+      <form
+        novalidate=""
+        className="space-y-6 ng-untouched ng-pristine ng-valid"
+      >
+        <div>
+          <label for="name" className="text-sm">
+            Full name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder=""
+            className="w-full p-3 rounded border-2 border-gray-400 "
+          />
+        </div>
+        <div>
+          <label for="email" className="text-sm">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-full p-3 rounded border-2 border-gray-400"
+          />
+        </div>
+        <div>
+          <label for="message" className="text-sm">
+            Message
+          </label>
+          <textarea
+            id="message"
+            rows="3"
+            className="w-full p-3 rounded border-2 border-gray-400"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-violet-500 text-gray-900"
+        >
+          Send Message
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,9 +21,9 @@ const Home = () => {
               <span className="dark:text-violet-400"> Quiz</span>
             </h1>
             <p className="mt-6 mb-8 text-lg sm:mb-12">
-              Dictum aliquam porta in condimentum ac integer
+              We’ve even made it our mission to get as many
               <br className="hidden md:inline lg:hidden" />
-              turpis pulvinar, est scelerisque ligula sem
+              people quizzing as we possibly can.
             </p>
             <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
               <Link
@@ -48,9 +48,12 @@ const Home = () => {
       <section className="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100 md:mt-0 lg:mt-20">
         <div className="container p-6 mx-auto space-y-8">
           <div className="space-y-2 text-center">
-            <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
+            <h2 className="text-3xl font-bold">
+              General Knowledge Quiz Questions
+            </h2>
             <p className="font-serif text-sm dark:text-gray-400">
-              Qualisque erroribus usu at, duo te agam soluta mucius.
+              A good general knowledge quiz has a lot to offer. They’re fun,
+              keep your brain sharp and best of all, they suit any occasion.
             </p>
           </div>
           <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
